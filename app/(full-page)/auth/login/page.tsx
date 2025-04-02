b8db8c74dc76f86b2aba265be7a6164239039e42
nuaip <mebarkinouaim@gmail.com>
2025-04-02 13:11:49 +0100

    fix

--- app/(full-page)/auth/login/page.tsx
+++ app/(full-page)/auth/login/page.tsx
@@ -62,14 +62,9 @@ const LoginPage = () => {
             style={{ borderRadius: "53px" }}
           >
             <div className="text-center mb-5">
-              <img
-                src="/demo/images/login/avatar.png"
-                alt="Image"
-                height="50"
-                className="mb-3"
-              />
+              
               <div className="text-900 text-3xl font-medium mb-3">
-                Welcome, Isabel!
+                Welcome!
               </div>
               <span className="text-600 font-medium">Sign in to continue</span>
             </div>
